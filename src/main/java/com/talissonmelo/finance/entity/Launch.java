3d1fca2f5dfa2f7d0cf342550d2a77a4f961ba5e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-21 13:44:52 -0300

    Utilizando biblioteca Java Lombok

--- src/main/java/com/talissonmelo/finance/entity/Launch.java
+++ src/main/java/com/talissonmelo/finance/entity/Launch.java
@@ -3,6 +3,7 @@ package com.talissonmelo.finance.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -17,106 +18,46 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.talissonmelo.finance.entity.enums.StatusLaunch;
 import com.talissonmelo.finance.entity.enums.TypeLaunch;
 
+import lombok.Builder;
+import lombok.Data;
+
 @Entity
 @Table(name = "launch")
+@Data
+@Builder
 public class Launch implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@Column(name = "month")
 	private Integer month;
+	
+	@Column(name = "year")
 	private Integer year;
+	
+	@Column(name = "value")
 	private Double value;
 
 	@JsonFormat(pattern = "dd/MM/yyyy", timezone = "America/Sao_Paulo")
+	@Column(name = "date")
 	private Date date;
 	
 	@Enumerated(value = EnumType.STRING)
+	@Column(name = "type")
 	private TypeLaunch type;
 	
 	@Enumerated(value = EnumType.STRING)
+	@Column(name = "status")
 	private StatusLaunch status;
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
-	
-	public Launch() {
-		
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Integer getMonth() {
-		return month;
-	}
-
-	public void setMonth(Integer month) {
-		this.month = month;
-	}
-
-	public Integer getYear() {
-		return year;
-	}
-
-	public void setYear(Integer year) {
-		this.year = year;
-	}
-
-	public Double getValue() {
-		return value;
-	}
-
-	public void setValue(Double value) {
-		this.value = value;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public TypeLaunch getType() {
-		return type;
-	}
-
-	public void setType(TypeLaunch type) {
-		this.type = type;
-	}
-
-	public StatusLaunch getStatus() {
-		return status;
-	}
-
-	public void setStatus(StatusLaunch status) {
-		this.status = status;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
 	
 }
