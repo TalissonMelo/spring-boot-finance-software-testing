a981ab717333b83d9b37fa7cedad126b783570ea
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-25 09:37:30 -0300

    Implementando metodos do LancamentoService

--- src/main/java/com/talissonmelo/finance/services/impl/LaunchServiceImpl.java
+++ src/main/java/com/talissonmelo/finance/services/impl/LaunchServiceImpl.java
@@ -1,51 +1,75 @@
 package com.talissonmelo.finance.services.impl;
 
 import java.util.List;
+import java.util.Objects;
 
+import javax.transaction.Transactional;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.ExampleMatcher.StringMatcher;
 import org.springframework.stereotype.Service;
 
 import com.talissonmelo.finance.entity.Launch;
 import com.talissonmelo.finance.entity.enums.StatusLaunch;
+import com.talissonmelo.finance.exceptions.ErrorAuthenticateException;
+import com.talissonmelo.finance.exceptions.businessRuleException;
 import com.talissonmelo.finance.repository.LaunchRepository;
 import com.talissonmelo.finance.services.LaunchService;
 
 @Service
 public class LaunchServiceImpl implements LaunchService {
-	
+
 	private LaunchRepository repository;
-	
+
 	public LaunchServiceImpl(LaunchRepository repository) {
 		this.repository = repository;
 	}
 
 	@Override
+	@Transactional
 	public Launch insert(Launch launch) {
-		// TODO Auto-generated method stub
-		return null;
+		return repository.save(launch);
 	}
 
 	@Override
+	@Transactional
 	public Launch update(Launch launch) {
-		// TODO Auto-generated method stub
-		return null;
+		Objects.requireNonNull(launch.getId());
+		return repository.save(launch);
 	}
 
 	@Override
+	@Transactional
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-		
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ErrorAuthenticateException("Id não encontrado : " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new businessRuleException(
+					"Não pode deletar esses dados por terem persistencia com outros dados no banco");
+		}
+
 	}
 
 	@Override
 	public List<Launch> findAll(Launch launchFilter) {
-		// TODO Auto-generated method stub
-		return null;
+		Example<Launch> example = Example.of(launchFilter, ExampleMatcher
+				.matching()
+				.withIgnoreCase()
+				.withStringMatcher(StringMatcher.CONTAINING));
+		
+		return repository.findAll(example);
 	}
 
 	@Override
+	@Transactional
 	public void updateStatus(Launch launch, StatusLaunch status) {
-		// TODO Auto-generated method stub
-		
+		launch.setStatus(status);
+		update(launch);
 	}
 
 }
