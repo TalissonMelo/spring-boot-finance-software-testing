076b86ac8ec74d502783e3fc06eff4f09613db7e
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-28 09:57:19 -0300

    Implementado endpoints filtrar/buscar e deletar lancamentos

--- src/main/java/com/talissonmelo/finance/resources/LaunchResource.java
+++ src/main/java/com/talissonmelo/finance/resources/LaunchResource.java
@@ -1,12 +1,18 @@
 package com.talissonmelo.finance.resources;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talissonmelo.finance.entity.Launch;
@@ -14,6 +20,7 @@ import com.talissonmelo.finance.entity.User;
 import com.talissonmelo.finance.entity.dto.LaunchDTO;
 import com.talissonmelo.finance.entity.enums.StatusLaunch;
 import com.talissonmelo.finance.entity.enums.TypeLaunch;
+import com.talissonmelo.finance.exceptions.ErrorAuthenticateException;
 import com.talissonmelo.finance.exceptions.businessRuleException;
 import com.talissonmelo.finance.services.LaunchService;
 import com.talissonmelo.finance.services.UserService;
@@ -55,6 +62,42 @@ public class LaunchResource {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
 		}).orElseGet(() -> new ResponseEntity<String>("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		try {
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+		} catch (ErrorAuthenticateException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		} catch (businessRuleException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@GetMapping
+	public ResponseEntity<?> findAll(
+			@RequestParam(value = "description", required = false) String description,
+			@RequestParam(value = "month", required = false) Integer month,
+			@RequestParam(value = "year", required = false) Integer year, 
+			@RequestParam("user") Long user) {
+
+		Launch launchFilter = new Launch();
+		launchFilter.setDescription(description);
+		launchFilter.setMonth(month);
+		launchFilter.setYear(year);
+
+		Optional<User> use = userService.findUserById(user);
+		if (use.isPresent()) {
+			return ResponseEntity.badRequest().body("Usuário não encontrado com Id " + user);
+		} else {
+			launchFilter.setUser(use.get());
+		}
+
+		List<Launch> list = service.findAll(launchFilter);
+		return ResponseEntity.ok().body(list);
+
 	}
 
 	private Launch fromDTO(LaunchDTO dto) {
