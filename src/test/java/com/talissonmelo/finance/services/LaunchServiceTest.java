36a259458b9f66fc6ead9c9af729d04913179400
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-29 10:17:52 -0300

    Testando atualizacao de lancamentos

--- src/test/java/com/talissonmelo/finance/services/LaunchServiceTest.java
+++ src/test/java/com/talissonmelo/finance/services/LaunchServiceTest.java
@@ -53,11 +53,44 @@ public class LaunchServiceTest {
 		// cenário
 		Launch launch = LaunchRespositoryTest.createLaunch();
 		Mockito.doThrow(businessRuleException.class).when(service).validate(launch);
-		
-		//execução 
+
+		// execução
 		Assertions.catchThrowableOfType(() -> service.insert(launch), businessRuleException.class);
-		
-		//verificação
+
+		// verificação
+		Mockito.verify(repository, Mockito.never()).save(launch);
+
+	}
+
+	@Test
+	public void updateLaunchSuccess() {
+
+		// cenário
+		Launch launch = LaunchRespositoryTest.createLaunch();
+		launch.setId(1l);
+		launch.setStatus(StatusLaunch.PENDING);
+		launch.setDate(new Date());
+		Mockito.doNothing().when(service).validate(launch);
+
+		Mockito.when(repository.save(launch)).thenReturn(launch);
+
+		// execução
+		service.insert(launch);
+
+		// verificação
+		Mockito.verify(repository, Mockito.times(1)).save(launch);
+	}
+
+	@Test
+	public void notUpdateLaunchErrorValidate() {
+
+		// cenário
+		Launch launch = LaunchRespositoryTest.createLaunch();
+
+		// execução
+		Assertions.catchThrowableOfType(() -> service.update(launch), NullPointerException.class);
+
+		// verificação
 		Mockito.verify(repository, Mockito.never()).save(launch);
 
 	}
