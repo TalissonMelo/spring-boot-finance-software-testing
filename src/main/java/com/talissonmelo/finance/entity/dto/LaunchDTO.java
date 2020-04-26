ea36c453e973f6684684f0ec3d76dd9ce74f366b
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-04-26 10:27:56 -0300

    Criando Construtor para LancamentoDTO

--- src/main/java/com/talissonmelo/finance/entity/dto/LaunchDTO.java
+++ src/main/java/com/talissonmelo/finance/entity/dto/LaunchDTO.java
@@ -1,10 +1,14 @@
 package com.talissonmelo.finance.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LaunchDTO {
 
 	private Long id;
